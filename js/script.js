08fae00af1619d81ca6b3dcd45e9b89ff0957e02
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2023-10-04 19:38:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// aos
+AOS.init({
+    duration:500,
+    delay:50,
+    offset:100,
+    easing:'linear'
+});
+
+// mix it up
+
+var mixer = mixitup('#selec', {
+    animation: {
+        duration: 300
+    }
+});
